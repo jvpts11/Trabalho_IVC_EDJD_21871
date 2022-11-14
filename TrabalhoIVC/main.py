79fec457d5563ec1dfc3014aa0af34f1efc41563
Dário Ribeiro <ninjago00700@gmail.com>
2022-11-14 16:13:16 +0000

    pó crl
    Why are u gae

--- TrabalhoIVC/main.py
+++ TrabalhoIVC/main.py
@@ -60,6 +60,8 @@ class Ball(GameObject):
             if isinstance(game_object, Brick):
                 game_object.hit()
 
+def cancro_terminal():
+    print("wy are u gae")
 
 class Paddle(GameObject):
     def __init__(self, canvas, x, y):
