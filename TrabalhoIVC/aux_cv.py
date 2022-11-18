8a6cb9f264f23a2a1473aadb8e71b7c2b7871111
jvpts11 <jvptsyt11@gmail.com>
2022-11-18 16:41:52 +0000

    explicacaozinha

--- TrabalhoIVC/aux_cv.py
+++ TrabalhoIVC/aux_cv.py
@@ -27,16 +27,16 @@ def cv_update(game):
 
 
 def cv_process(image, game):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv,color_lower_limit,color_upper_limit )
-    half_size = get_image_half_size(image)
-    converted_half_size = int(half_size)
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    image_copy = image.copy()
-    if len(contours) != 0:
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) #conversão de cor de BGR para HSV
+    mask = cv2.inRange(hsv,color_lower_limit,color_upper_limit ) #segmentação da cor verde na imagem
+    half_size = get_image_half_size(image) #obteção da metade do tamanho da imagemp ara depois ser usada no jogo
+    converted_half_size = int(half_size) #conversão do valor de float para int
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #criação dos contornos e detecção dos mesmos
+    image_copy = image.copy() #cópia da imagem original para não haver erros
+    if len(contours) != 0: #caso haja algum contorno detectado na imagem
         for contour in contours:
-            if cv2.contourArea(contour) > 500:
-                x, y, w, h = cv2.boundingRect(contour)
+            if cv2.contourArea(contour) > 500: #detecção de contornos de mais de 500 pixels para evitar detectar qualquer ruído como contorno
+                x, y, w, h = cv2.boundingRect(contour) #atribuição
                 cv2.rectangle(image_copy, (x,y), (x + w, y + h),(0, 255, 0), 2)
                 if converted_half_size < x:
                     game.paddle.move(-10)
