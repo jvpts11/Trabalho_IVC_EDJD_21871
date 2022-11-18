fca99f6923bfd39ba6fee3139d1cd00dc73fccd6
jvpts11 <jvptsyt11@gmail.com>
2022-11-18 14:29:19 +0000

    it works
    i have crippling depression

--- TrabalhoIVC/aux_cv.py
+++ TrabalhoIVC/aux_cv.py
@@ -29,7 +29,8 @@ def cv_update(game):
 def cv_process(image, game):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv,color_lower_limit,color_upper_limit )
-    half_size = get_image_half_size(mask)
+    half_size = get_image_half_size(image)
+    converted_half_size = int(half_size)
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     image_copy = image.copy()
     if len(contours) != 0:
@@ -37,7 +38,11 @@ def cv_process(image, game):
             if cv2.contourArea(contour) > 500:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(image_copy, (x,y), (x + w, y + h),(0, 255, 0), 2)
-                game.paddle.move(x)
+                if converted_half_size < x:
+                    game.paddle.move(-10)
+                if converted_half_size > x:
+                    game.paddle.move(10)
+                print(converted_half_size)
                 print(x)
     cv2.imshow("Original", image_copy)
     cv2.imshow("Processed", mask)
